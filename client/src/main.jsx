64e0d9039ef37330396cc012534f62ea7c79f2b9
shobuj <shobujd6@gmail.com>
2023-10-05 23:33:11 +0600

    modified main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,4 +1,4 @@
-
+import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
@@ -11,10 +11,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <AuthProvider>
     <SearchProvider>
       <BrowserRouter>
-        <App />
+        {/* <React.StrictMode> */}
+          <App />
+        {/* </React.StrictMode> */}
       </BrowserRouter>
     </SearchProvider>
   </AuthProvider>
 );
-
-
